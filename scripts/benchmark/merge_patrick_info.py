1f5da1bea709e89e3ddc2b0298b5c5d499b959f7
ctokheim <collintokheim@gmail.com>
2017-08-18 21:42:57 -0400

    Fixed merging script to include all necessary columns and avoid duplicates

--- scripts/benchmark/merge_patrick_info.py
+++ scripts/benchmark/merge_patrick_info.py
@@ -28,15 +28,30 @@ def parse_arguments():
 def main(opts):
     # read data
     df = pd.read_table(opts['input'])
-    coord_df = pd.read_table(opts['genomic_coord'])
+    coord_df = pd.read_table(opts['genomic_coord']).drop_duplicates()
+
+    # keep only missense mutations
+    df = df[df.aa.str.match('^[A-Z][0-9]+[A-Z]$')]
 
     # merge the info
     mut_info_df = coord_df['coordinates(gDNA/cDNA/protein)'].str.split('/', expand=True).rename(columns={ 0: 'gDNA', 1: 'cDNA', 2: 'protein'})
-    df['Chromosome'] = mut_info_df.gDNA.str.extract('^[chr]+([0-9]+)', expand=False)
-    df['Start_Position'] = mut_info_df.gDNA.str.extract(':g.([0-9]+)', expand=False)
-    df['Reference_Allele'] = mut_info_df.gDNA.str.extract('[0-9]+([ACTG])>', expand=False)
-    df['Tumor_Seq_Allele2'] = mut_info_df.gDNA.str.extract('[0-9]+[ACTG]>([ACTG])$', expand=False)
+    mut_info_df['Chromosome'] = mut_info_df.gDNA.str.extract('^[chr]+([0-9XY]+)', expand=False)
+    mut_info_df['Start_Position'] = mut_info_df.gDNA.str.extract(':g.([0-9]+)', expand=False)
+    mut_info_df['End_Position'] = mut_info_df['Start_Position']
+    mut_info_df['Reference_Allele'] = mut_info_df.gDNA.str.extract('[0-9]+([ACTG])>', expand=False)
+    mut_info_df['Tumor_Seq_Allele2'] = mut_info_df.gDNA.str.extract('[0-9]+[ACTG]>([ACTG])$', expand=False)
+    tmp = coord_df.input.str.split(':', expand=True).rename(columns={0: 'gene', 1: 'aa'})
+    mut_info_df['gene'] = tmp['gene']
+    mut_info_df['aa'] = tmp['aa']
+
+    # merge in the information
+    all_cols = ['Chromosome', 'Start_Position', 'End_Position', 'Reference_Allele',
+                'Tumor_Seq_Allele2', 'gene', 'aa']
+    df = pd.merge(df, mut_info_df[all_cols], on=['gene', 'aa'], how='left')
+
+    # Fix some of the columns
     df['HGVSp_Short'] = 'p.' + df['aa']
+    df['Strand'] = '+'
     df = df.rename(columns={'gene': 'Hugo_Symbol', 'consensus_high_call_DF0629': 'status'})
 
     # save merged results
