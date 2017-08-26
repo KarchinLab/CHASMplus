175a0b68e1c3815e29a7fc23095b67229a3459c2
ctokheim <collintokheim@gmail.com>
2017-08-26 16:34:13 -0400

    Make test prediction code into a script and removed extraneous other code

--- chasm2/r/test.R
+++ chasm2/r/test.R
@@ -1,9 +1,32 @@
-library(randomForest)
+suppressPackageStartupMessages(library(randomForest))
+set.seed(101)
+
+# get command line args
+if ("getopt" %in% rownames(installed.packages())){
+  # get command line arguments
+  library(getopt)
+  spec <- matrix(c(
+    'input', 'i', 1, 'character',
+    'trained_mod', 't', 1, 'character',
+    'output', 'o', 1, 'character',
+    'help', 'h', 0, 'logical'
+  ), byrow=TRUE, ncol=4)
+  opt = getopt(spec)
+  # print out help msg
+  if ( !is.null(opt$help) ) {
+    cat(getopt(spec, usage=TRUE));
+    q(status=1);
+  } else if (is.null(opt$output)){
+    opt <- list(ARGS=NULL)
+  }
+} else {
+  opt <- list(ARGS=NULL)
+}
+# load model
+load(opts$trained_mod)
 
 # read/process feature data frame
-#featDf <- read.delim("../output/mc3/features.txt")
-featDf <- read.delim("../output/gene_features_1_26_2017/features_merged.txt")
-#featDf <- read.delim("../output/gene_features_1_26_2017/snvbox/features.txt")
+featDf <- read.delim(opt$input)
 row.names(featDf) <- featDf$UID
 featDf <- featDf[!duplicated(featDf$ID),]
 colSelect <- -which(names(featDf) %in% c("UID", "ID"))
@@ -32,65 +55,9 @@ for(i in featureCols){
 }
 
 # perform random forest
-#rf.model <- randomForest(class ~ . - gene - strata, 
-                         #strata=featDf$strata, sampsize=geneCounts,
-                         #sampsize=c(6000, 6000),
-                         #data=featDf)
-rf.model <- randomForest(class ~ . - gene, 
-                         #strata=featDf$strata, sampsize=geneCounts,
-                         #sampsize=c(6000, 6000),
+rf.model <- randomForest(class ~ . - gene - strata, 
+                         strata=featDf$strata, sampsize=geneCounts,
                          data=featDf)
 result <- predict(rf.model, type="prob")
 finalDf <- cbind(featDf, result)
 write.table(finalDf, "../output/gene_features_1_26_2017/oob_predictions.txt", sep='\t')
-
-# get the list of genes
-driverGenes <- sample(unique(featDf[featDf$class=="driver",]$gene))
-driverLen <- length(driverGenes)
-passengerGenes <- sample(unique(featDf[featDf$class=="passenger",]$gene))
-passengerLen <- length(passengerGenes)
-
-# add columns for driver/passenger score
-featDf[,c('driver', 'passenger')] <- 0
-
-# handle indexing
-startIxD <- 1
-startIxP <- 1
-nfold <- 10
-for (i in 1:nfold){
-  # get list of genes to use for drivers
-  endIxD <- i * driverLen / nfold 
-  testIxD <- startIxD:endIxD
-  testDGenes <- as.character(driverGenes[testIxD])
-  trainDGenes <- as.character(driverGenes[-testIxD])
-  startIxD <- endIxD + 1
-
-  # get list of genes to use for passengers
-  endIxP <- i * passengerLen / nfold 
-  testIxP <- startIxP:endIxP
-  testPGenes <- as.character(passengerGenes[testIxP])
-  trainPGenes <- as.character(passengerGenes[-testIxP])
-  startIxP <- endIxP + 1
-
-  # combine list of genes
-  trainGenes <- c(trainPGenes, trainDGenes)
-  testGenes <- c(testPGenes, testDGenes)
-
-  # get train and test
-  trainDf <- featDf[featDf$gene %in% trainGenes,]
-  testDf <- featDf[featDf$gene %in% testGenes,]
-  # make sure strata has correct factor levels for training
-  #tmp <- as.character(trainDf$strata)
-  #trainDf$strata <- factor(tmp, levels=c(trainDGenes, 'not_driver'))
-
-  # perform training
-  rf.model <- randomForest(class ~ . - gene - driver - passenger, 
-                           #strata=trainDf$strata, sampsize=geneCounts[c(trainDGenes, 'not_driver')],
-                           #sampsize=c(6000, 6000),
-                           data=trainDf)
-
-  # perform test
-  result <- predict(rf.model, testDf, type="prob")
-  featDf[rownames(result), c('driver', 'passenger')] <- result
-
-}
