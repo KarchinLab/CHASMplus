4bb383288a9b9e471758206dcdf1104f3b3fa514
ctokheim <collintokheim@gmail.com>
2017-09-28 10:11:08 -0400

    Updated subsample script

--- scripts/subsample/subsample_run.sh
+++ scripts/subsample/subsample_run.sh
@@ -1,6 +1,6 @@
 mydir=$1
 n=30
 for i in {0..29} ; do
-    snakemake -s Snakefile chasm2_pretrained -p -j 999 -w 100 --max-jobs-per-second 1 --config  mutations="$mydir/sample$i/mutations.maf" output_dir="$mydir/sample$i" mutsigcv_dir="tmp2/mutsigcv_formatted" trained_model="/projects/CHASM2_results/output/snvbox_hg38_6_13_2017/cv_trained_no_gtex_no_exac_hotmaps_multip" trained_classifier="output/cancer_types/2020plus_10k.Rdata" --cluster-config cluster.yaml --cluster "qsub -cwd -pe smp {threads} -l mem_free={cluster.mem},h_vmem={cluster.vmem} -v PATH=$PATH -o $mydir/sample$i/sge_log -e $mydir/sample$i/sge_log"
+    snakemake -s Snakefile chasm2_pretrained -p -j 999 -w 100 --max-jobs-per-second 1 --config  mutations="$mydir/sample$i/mutations.maf" output_dir="$mydir/sample$i" mutsigcv_dir="output/mutsigcv_formatted" trained_model="/projects/CHASM2/output/pancancer_v8_regular/cv_trained_model" trained_classifier="output/pancancer_v8_regular/2020plus.Rdata" --cluster-config cluster.yaml --cluster "qsub -cwd -pe smp {threads} -l mem_free={cluster.mem},h_vmem={cluster.vmem} -v PATH=$PATH -o $mydir/sample$i/sge_log -e $mydir/sample$i/sge_log"
     sleep 5
 done
